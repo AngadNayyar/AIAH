9ca2df3454448a1c4f9992e6583efdcdf520dfaa
ChristinaBell <cbel296@aucklanduni.ac.nz>
2017-05-30 17:25:52 +1200

    update from master

--- src/main/java/net/socialgamer/cah/cardcast/CardcastWhiteCard.java
+++ src/main/java/net/socialgamer/cah/cardcast/CardcastWhiteCard.java
@@ -15,6 +15,106 @@ public class CardcastWhiteCard extends WhiteCard {
     this.deckId = deckId;
   }
 
+  // This wont be used
+
+  public void setDeathHarm(double deathHarm) {
+
+  }
+
+  public void setRandom(double random) {
+
+  }
+
+  public void setPolitical(double political) {
+
+  }
+
+  public void setHuman(double human) {
+
+  }
+
+  public void setReligion(double religion) {
+
+  }
+
+  public void setControversial(double controversial) {
+
+  }
+
+  public void setGross(double gross) {
+
+  }
+
+  public void setScientific(double scientific) {
+
+  }
+
+  public void setRacism(double racism) {
+
+  }
+
+  public void setLocation(double location) {
+
+  }
+
+  public void setCelebrity(double celebrity) {
+
+  }
+
+  public void setSexual(double sexual) {
+
+  }
+
+  public double getDeathHarm() {
+    return 0;
+  }
+
+  public double getSexual() {
+    return 0;
+  }
+
+  public double getPolitical() {
+    return 0;
+  }
+
+  public double getHuman() {
+    return 0;
+  }
+
+  public double getReligion() {
+    return 0;
+  }
+
+  public double getControversial() {
+    return 0;
+  }
+
+  public double getGross() {
+    return 0;
+  }
+
+  public double getScientific() {
+    return 0;
+  }
+
+  public double getRacism() {
+    return 0;
+  }
+
+  public double getLocation() {
+    return 0;
+  }
+
+  public double getCelebrity() {
+    return 0;
+  }
+
+  public double getRandom() {
+    return 0;
+  }
+
+
+
   @Override
   public int getId() {
     return id;
